b4be9ba8551f8cfedfbe92be3dfc0ce0170c0ae2
clach04 <clach04@gmail.com>
2022-07-24 13:56:12 -0700

    Start of #2 - browser clipboard support

--- clipboardconfusion.py
+++ clipboardconfusion.py
@@ -235,6 +235,41 @@ function init() {
 
 }
 
+    function fallbackCopyTextToClipboard(text) {
+        var textArea = document.createElement("textarea");
+        textArea.value = text;
+
+        // Avoid scrolling to bottom
+        textArea.style.top = "0";
+        textArea.style.left = "0";
+        textArea.style.position = "fixed";
+
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+
+        try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'successful' : 'unsuccessful';
+            console.log('Fallback: Copying text command was ' + msg);
+        } catch (err) {
+            console.error('Fallback: Oops, unable to copy', err);
+        }
+
+        document.body.removeChild(textArea);
+    }
+    function copyTextToClipboard(text) {
+        if (!navigator.clipboard) {
+            fallbackCopyTextToClipboard(text);
+            return;
+        }
+        navigator.clipboard.writeText(text).then(function() {
+            console.log('Async: Copying to clipboard was successful!');
+        }, function(err) {
+            console.error('Async: Could not copy text: ', err);
+        });
+    }
+
 window.onload=init; /* <body onload="init()"> */
 </script>
     """
@@ -256,6 +291,8 @@ window.onload=init; /* <body onload="init()"> */
     """
     )
 
+    result.append("""<button class="js-copy-to-clipboard">Copy to (browser) clipboard</button><br />""")
+
     result.append(
         """
     <form accept-charset="utf-8" action="setclipboard" method="POST" id="myform" name="myform">
@@ -270,6 +307,18 @@ window.onload=init; /* <body onload="init()"> */
         <br />
         <input type="submit" value="Update clipboard"/>
     </form>
+    """
+    )
+    result.append(
+        """
+    <script>
+
+    var copyClipBtn = document.querySelector('button.js-copy-to-clipboard');
+
+    copyClipBtn.addEventListener('click', function(event) {
+        copyTextToClipboard('TODO actual content goes here');
+    });
+    </script>
     """
     )
     result.append("""</body>""")
